b21dabd3c10af59e279b3e2268705f4bec1d2f34
ArturchiKGilyazov <a.gilyazov@g.nsu.ru>
2022-10-14 17:30:32 +0700

    Rework 1_1, and do 1_2 lab

---
+++ 1lab/src/2lab.clj
@@ -0,0 +1,39 @@
+(def N 5)
+(def symbols ['a 'b 'c])
+(println N)
+(println symbols)
+(defn extend-word [word, alphabet, result-word]             ; создаём слово
+  (cond
+    (= (count alphabet) 0) result-word                      ; если алфавит кончился
+    (= (first word) (first alphabet)) (extend-word word (rest alphabet) result-word) ; скипаем повторяющуюся букву
+    :default (recur
+               word
+               (rest alphabet)
+               (conj result-word (cons (first alphabet) word))) ;конкатим букву к слову и вызываем рекурсивно добавление буквы к слову
+    )
+  )
+
+(defn extend-words [words, alphabet, result-words]          ; добавить новое слово
+  (if
+    (= (count words) 0) result-words                        ; если символов не осталось, то выводим слово обратно
+                        (recur
+                          (rest words)
+                          alphabet
+                          (concat result-words (extend-word (first words) alphabet (list))))
+
+                        )
+  )
+
+(defn make-words [alphabet max-length]
+  (if
+    (= max-length 0) (list (list))
+                     (extend-words (make-words alphabet (dec max-length)) alphabet (list))
+                     )
+  )
+
+(defn startFirstLab [list max-length]
+  (let [result (make-words list max-length)]
+    (println (str "result: ") result))
+  )
+
+(startFirstLab symbols N)
